bcdb8abaedd0c58422a095c206327eb41e7f1d79
Timothy Bird <timothymmbird@gmail.com>
2020-01-26 10:37:43 +0000

    added json file

--- EyeDocTrainer/Controllers/GameController.cs
+++ EyeDocTrainer/Controllers/GameController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using EyeDocTrainer.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace EyeDocTrainer.Controllers
 {
@@ -15,30 +17,34 @@ namespace EyeDocTrainer.Controllers
             return View();
         }
         
-        public IActionResult Scene(Scene model = null)
+        public IActionResult Scene(string? pathToScene)
         {
-            if(model.Description == null)
-            {
-                var decision1 = new Decision
-                {
-                    Text = "use glue",
-                    WillFail = true
-                };
-                var decision2 = new Decision
-                {
-                    Text = "use ALL the glue",
-                    WillFail = false
-                };
-                model = new Scene
-                {
-                    Timer = 0,
-                    Description = "This is the first decision",
-                    Decisions = new List<Decision>()
-                };
-                model.Decisions.Add(decision1);
-                model.Decisions.Add(decision2);
-            }
-            
+            Scene model = new Scene();
+
+            File(pathToScene);
+            Scene model = JsonConvert.DeserializeObject<Scene>(pathToScene);
+            //if (model.Description == null)
+            //{
+            //    var decision1 = new Decision
+            //    {
+            //        Text = "use glue",
+            //        WillFail = true
+            //    };
+            //    var decision2 = new Decision
+            //    {
+            //        Text = "use ALL the glue",
+            //        WillFail = false
+            //    };
+            //    model = new Scene
+            //    {
+            //        Timer = 0,
+            //        Description = "This is the first decision",
+            //        Decisions = new List<Decision>()
+            //    };
+            //    model.Decisions.Add(decision1);
+            //    model.Decisions.Add(decision2);
+            //}
+
             return View(model);
         }
 
